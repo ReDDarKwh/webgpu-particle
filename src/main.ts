b7187c2598887b950ca2278222e9ba86ba93ce71
William Henry <reddarkwh@gmail.com>
2024-01-15 18:31:18 -0500

    WIP

--- src/main.ts
+++ src/main.ts
@@ -1,112 +1,62 @@
-import './style.css'
-import shader from './shaders/shader.wgsl?raw'
-import stringToTemplateLiteral from './stringTemplate'
+import './style.css';
+import shader from './shaders/shader.wgsl?raw';
+import particleSimShader from './shaders/particleSim.wgsl?raw';
+import stringTemplate from './stringTemplate';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <canvas width="512" height="512"></canvas>
-  </div>
-`
-const canvas = document.querySelector("canvas");
+const { device, canvasFormat, context } = await Setup();
 
-if (!canvas) {
-  throw new Error("Yo! No canvas found.");
-}
-
-if (!navigator.gpu) {
-  throw new Error("WebGPU not supported on this browser.");
-}
-
-const adapter = await navigator.gpu.requestAdapter();
-if (!adapter) {
-  throw new Error("No appropriate GPUAdapter found.");
-}
-
-const device = await adapter.requestDevice();
-
-const context = canvas.getContext("webgpu");
-
-if (!context) {
-  throw new Error("Yo! No Canvas GPU context found.");
-}
-const canvasFormat = navigator.gpu.getPreferredCanvasFormat();
-context.configure({
-  device: device,
-  format: canvasFormat,
+const particleShaderModule = device.createShaderModule({
+  label: "Particle shader",
+  code: stringTemplate(shader, {})
 });
 
-const GRID_SIZE = 32;
-
-// Create a uniform buffer that describes the grid.
-const uniformArray = new Float32Array([GRID_SIZE, GRID_SIZE]);
-const uniformBuffer = device.createBuffer({
-  label: "Grid Uniforms",
-  size: uniformArray.byteLength,
-  usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
-});
-device.queue.writeBuffer(uniformBuffer, 0, uniformArray);
-
-// Create an array representing the active state of each cell.
-const cellStateArray = new Uint32Array(GRID_SIZE * GRID_SIZE);
-
-// Create a storage buffer to hold the cell state.
-const cellStateStorage = device.createBuffer({
-  label: "Cell State",
-  size: cellStateArray.byteLength,
-  usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST,
-});
-// Mark every third cell of the grid as active.
-for (let i = 0; i < cellStateArray.length; i += 3) {
-  cellStateArray[i] = 1;
-}
-device.queue.writeBuffer(cellStateStorage, 0, cellStateArray);
-
-const vertices = new Float32Array([
-  //   X,    Y,
-  -0.8, -0.8, // Triangle 1 (Blue)
-  0.8, -0.8,
-  0.8, 0.8,
-
-  -0.8, -0.8, // Triangle 2 (Red)
-  0.8, 0.8,
-  -0.8, 0.8,
-]);
-
-const vertexBuffer = device.createBuffer({
-  label: "Cell vertices",
-  size: vertices.byteLength,
-  usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
+const particleComputeShaderModule = device.createShaderModule({
+  label:"Compute shader", 
+  code: stringTemplate(particleSimShader, {})
 });
 
-device.queue.writeBuffer(vertexBuffer, 0, vertices);
-
-const vertexBufferLayout: GPUVertexBufferLayout = {
-  arrayStride: 8,
-  attributes: [{
-    format: "float32x2",
-    offset: 0,
-    shaderLocation: 0, // Position, see vertex shader
-  }],
-};
-
-
-let test = 2;
-
-const cellShaderModule = device.createShaderModule({
-  label: "Cell shader",
-  code: shader
+const bindGroupLayout = device.createBindGroupLayout({
+  entries: [
+    {
+      binding: 0,
+      visibility: GPUShaderStage.COMPUTE | GPUShaderStage.VERTEX,
+      buffer: {
+        type: "storage"
+      }
+    },
+    {
+      binding: 0,
+      visibility: GPUShaderStage.COMPUTE,
+      buffer: {
+        type: "read-only-storage"
+      }
+    }
+  ]
 });
 
-const cellPipeline = device.createRenderPipeline({
+const pipelineLayout = device.createPipelineLayout({
+  bindGroupLayouts:[
+    bindGroupLayout
+  ]
+});
+
+const computePipeline = device.createComputePipeline({
+  layout: pipelineLayout,
+  compute: {
+    module : particleComputeShaderModule,
+    entryPoint : "updateParticle"
+  }
+})
+
+const renderPipeline = device.createRenderPipeline({
   label: "Cell pipeline",
-  layout: "auto",
+  layout: pipelineLayout,
   vertex: {
-    module: cellShaderModule,
-    entryPoint: "vertexMain",
-    buffers: [vertexBufferLayout]
+    module: particleShaderModule,
+    entryPoint: "vertexMain"
   },
   fragment: {
-    module: cellShaderModule,
+    module: particleShaderModule,
     entryPoint: "fragmentMain",
     targets: [{
       format: canvasFormat
@@ -116,24 +66,14 @@ const cellPipeline = device.createRenderPipeline({
 
 const bindGroup = device.createBindGroup({
   label: "Cell renderer bind group",
-  layout: cellPipeline.getBindGroupLayout(0),
-  entries: [{
-    binding: 0,
-    resource: { buffer: uniformBuffer }
-  }, {
-
-    binding: 1,
-    resource: { buffer: cellStateStorage }
-  }],
+  layout: renderPipeline.getBindGroupLayout(0),
+  entries: [],
 });
 
-let step = 0;
-
 async function update(time: number): Promise<void> {
-  step++
-
+  
   const encoder = device.createCommandEncoder();
-
+  
   const pass = encoder.beginRenderPass({
     colorAttachments: [{
       view: context!.getCurrentTexture().createView(),
@@ -142,16 +82,49 @@ async function update(time: number): Promise<void> {
       storeOp: "store",
     }]
   });
-
-  pass.setPipeline(cellPipeline);
-  pass.setVertexBuffer(0, vertexBuffer);
-  pass.setBindGroup(0, bindGroup);
-  pass.draw(vertices.length / 2, GRID_SIZE * GRID_SIZE); // 6 vertices
+  
+  pass.setPipeline(renderPipeline);
+  pass.draw(6); // 6 vertices
   pass.end();
-
+  
   device.queue.submit([encoder.finish()]);
-
+  
   requestAnimationFrame(update)
 }
-
 requestAnimationFrame(update)
+
+async function Setup() {
+  document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <canvas width="512" height="512"></canvas>
+  </div>
+`;
+  const canvas = document.querySelector("canvas");
+
+  if (!canvas) {
+    throw new Error("Yo! No canvas found.");
+  }
+
+  if (!navigator.gpu) {
+    throw new Error("WebGPU not supported on this browser.");
+  }
+
+  const adapter = await navigator.gpu.requestAdapter();
+  if (!adapter) {
+    throw new Error("No appropriate GPUAdapter found.");
+  }
+
+  const device = await adapter.requestDevice();
+
+  const context = canvas.getContext("webgpu");
+
+  if (!context) {
+    throw new Error("Yo! No Canvas GPU context found.");
+  }
+  const canvasFormat = navigator.gpu.getPreferredCanvasFormat();
+  context.configure({
+    device: device,
+    format: canvasFormat,
+  });
+  return { device, canvasFormat, context };
+}
--- src/stringTemplate.ts
+++ src/stringTemplate.ts
@@ -1,16 +1,5 @@
 export default function stringTemplate(str: string, obj: any) {
-  
-
-    const regex = /\${(\w+)}/gm;
-
-    const res = regex.exec(str);
-
-    res?.forEach(match => {
-        
+    return str.replace(/\${(\w+)}/gm, (_match, p1) => {
+        return obj[p1];
     });
-
-    return str.replace(/\${(\w+)}/gm, )
-
-
-
 }
